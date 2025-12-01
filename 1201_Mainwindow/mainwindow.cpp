80f85e80a78a0743e8e023c2444b1857bb26e0e1
HuaMa001 <67962882+HuaMa001@users.noreply.github.com>
2025-12-01 17:18:03 +0800

    dark_mode

--- 1201_Mainwindow/mainwindow.cpp
+++ 1201_Mainwindow/mainwindow.cpp
@@ -1,9 +1,29 @@
 #include "mainwindow.h"
+#include <QPalette>
+#include <QColor>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     setupUi(this);
+    // 設定暗色調
+    QPalette darkPalette;
+
+    darkPalette.setColor(QPalette::Window, QColor(53,53,53));
+    darkPalette.setColor(QPalette::WindowText, Qt::white);
+    darkPalette.setColor(QPalette::Base, QColor(42,42,42));
+    darkPalette.setColor(QPalette::AlternateBase, QColor(66,66,66));
+    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+    darkPalette.setColor(QPalette::Text, Qt::white);
+    darkPalette.setColor(QPalette::Button, QColor(53,53,53));
+    darkPalette.setColor(QPalette::ButtonText, Qt::white);
+    darkPalette.setColor(QPalette::BrightText, Qt::red);
+
+    darkPalette.setColor(QPalette::Highlight, QColor(42,130,218));
+    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+
+    qApp->setPalette(darkPalette);
 }
 
 MainWindow::~MainWindow() {}
